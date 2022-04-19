d0dc7a916e673a7e6ede53420f2b5814b9a3688a
Malgorzata Becker <malgorzata.kot@jobcloud.ch>
2022-04-19 14:52:48 +0200

    initial commit

---
+++ consumers/ksql.py
@@ -0,0 +1,65 @@
+"""Configures KSQL to combine station and turnstile data"""
+import json
+import logging
+
+import requests
+
+import topic_check
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level='DEBUG')
+
+
+KSQL_URL = "http://localhost:8088"
+
+KSQL_STATEMENT = """
+TERMINATE CTAS_TURNSTILE_SUMMARY_0;
+DROP TABLE turnstile;
+DROP TABLE turnstile_summary;
+
+CREATE TABLE turnstile (
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
+) WITH (
+    KAFKA_TOPIC='org.chicago.cta.station.turnstile.v1',
+    VALUE_FORMAT='avro',
+    KEY='station_id'
+);
+
+CREATE TABLE turnstile_summary
+WITH (VALUE_FORMAT='JSON') AS
+    SELECT station_id, COUNT(station_id) AS count
+    FROM turnstile
+    GROUP BY station_id;
+"""
+
+
+def execute_statement():
+    """Executes the KSQL statement against the KSQL API"""
+    if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
+        return
+
+    logging.debug("executing ksql statement...")
+
+    resp = requests.post(
+        f"{KSQL_URL}/ksql",
+        headers={"Content-Type": "application/vnd.ksql.v1+json",
+                 "Accept": "application/vnd.ksql.v1+json"},
+        data=json.dumps(
+            {
+                "ksql": KSQL_STATEMENT,
+                "streamsProperties": {"ksql.streams.auto.offset.reset": "earliest"},
+            }
+        ),
+    )
+
+    logging.debug(resp.content)
+
+    # Ensure that a 2XX status code was returned
+    resp.raise_for_status()
+
+
+if __name__ == "__main__":
+    execute_statement()
